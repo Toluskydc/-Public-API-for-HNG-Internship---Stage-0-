9adf9327eb67cb05bd8eb936cf4941a5ff6e80e6
Toluskydc <booktolusky@gmail.com>
2025-01-30 00:26:02 -0800

    modified main.py

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
+import os
+import uvicorn
+
 from fastapi import FastAPI, status
 from fastapi.middleware.cors import CORSMiddleware
 from datetime import datetime
@@ -17,6 +20,12 @@ app.add_middleware(
 async def get_info():
     return {
         "email": "booktolusky@gmail.com",
-        "current_datetime": datetime.utcnow().replace(microsecond=0).isoformat() + "Z",
+        "current_datetime": datetime.utcnow().isoformat(timespec="seconds") + "Z",
         "github_url": "https://github.com/Toluskydc/-Public-API-for-HNG-Internship---Stage-0-"
     }
+
+
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
